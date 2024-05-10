b86864909f8e3e68fb2c28fcfb61a00b13694605
comanderpotato <tomgolding2012@outlook.com>
2024-05-10 16:35:56 +1000

    Added to toms branch

---
+++ WeatherApp/Views/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  WeatherApp
+//
+//  Created by Tom Golding on 10/5/2024.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationView()
+}
